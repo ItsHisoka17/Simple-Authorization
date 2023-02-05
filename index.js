34ab324b4ac98c35a0bb17e63ca8d2a52a6e40ee
HashT9u <61894183+HashT9u@users.noreply.github.com>
2023-02-05 05:55:41 -0500

    SERVER IMPROVEMENTS

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const app = require('./src/server.js');
-
-app.listen(3000, function() {
-  console.log(`SERVER RUNNING ON PORT: ${this.address().port}\nACTIVE LISTENERS: ${app._router.stack.length}\nRoutes: ${app._router.stack.map((r)=> `${r.handle.name}`).join(' | ')}`)
-});
+const express = require('express')();
+(()=> {
+  new (require('./src/server'))(express);
+})();
