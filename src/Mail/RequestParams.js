5ab8d84ed0c682e45855cb3a9b84c8787fbd4bb4
Jxdn <61894183+ItsHisoka17@users.noreply.github.com>
2024-04-26 17:47:30 +0000

    JWT Auth Updates /MailingSystem

--- src/Mail/RequestParams.js
+++ src/Mail/RequestParams.js
@@ -1,6 +1,6 @@
 let authParams = {
-  clientEmail: process.env.client_email,
-  clientPass: process.env.client_pass,
+  clientEmail: process.env["client_email"],
+  clientPass: process.env["client_pass"],
 };
 
 module.exports = { authParams };
